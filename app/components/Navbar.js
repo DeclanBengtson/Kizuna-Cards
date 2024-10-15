0099f7c303365f7b5890346f62089555543c5d03
Declan Bengtson <n11079550@qut.edu.au>
2024-10-16 07:26:07 +1000

    Updated Navbar

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -7,11 +7,9 @@ export default function Navbar() {
     <nav className="navbar">
       <Link href="/" className="navbar-logo">Couples Questions</Link>
       <div className="navbar-links">
-        <Link href="/Subscription" className="navbar-icon">Subscriptions</Link>
         <Link href="/Browser" className="navbar-icon">Browser</Link>
         <Link href="/login" className="navbar-icon">Login</Link>
-        <Link href="/register" className="navbar-icon">Register</Link>
-        
+        <Link href="/Subscription" className="navbar-icon">Premium</Link>
       </div>
     </nav>
   );
