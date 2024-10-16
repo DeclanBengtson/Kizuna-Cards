cf4fe18f249f64624a75b2ec50726ca186146b1a
Declan Bengtson <n11079550@qut.edu.au>
2024-10-16 19:23:22 +1000

    Fixed checkout

--- app/api/subscriptions/create-checkout-session/route.js
+++ app/api/subscriptions/create-checkout-session/route.js
@@ -1,25 +1,33 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-08-27',
 });
 
-export default async function handler(req = NextApiRequest, res = NextApiResponse) {
-  const { priceId } = req.body;
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [{
-        price: priceId,
-        quantity: 1,
-      }],
-      mode: 'subscription',
-      success_url: `${req.headers.origin}/success`,
-      cancel_url: `${req.headers.origin}/cancel`,
-    });
-    res.status(200).json({ id: session.id });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { priceId } = req.body;
+
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            price: priceId,
+            quantity: 1,
+          },
+        ],
+        mode: 'subscription',
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/cancel`,
+      });
+
+      res.status(200).json({ id: session.id });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
