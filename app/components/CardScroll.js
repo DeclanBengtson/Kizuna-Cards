e851ecd666add9dcf26b8f1c8e8ec7135875bc6a
Declan Bengtson <n11079550@qut.edu.au>
2024-10-17 09:40:50 +1000

    Infinite loop updated

--- app/components/CardScroll.js
+++ app/components/CardScroll.js
@@ -2,10 +2,13 @@ import Image from 'next/image';
 import './CardScroll.css';
 
 const CardScroll = ({ cards }) => {
+  // Duplicate cards to create a seamless loop
+  const duplicatedCards = [...cards, ...cards];
+
   return (
     <div className="card-scroll">
       <div className="card-scroll-track">
-        {cards.map((card, index) => (
+        {duplicatedCards.map((card, index) => (
           <div key={index} className="card-scroll-item">
             <Image src={card.image} alt={card.alt} width={200} height={300} />
           </div>
