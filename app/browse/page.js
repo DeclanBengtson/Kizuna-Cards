7da5865b29826d906ddc76c034df6ac713965ef6
Declan Bengtson <n11079550@qut.edu.au>
2024-10-19 16:50:20 +1000

    Styling changes

--- app/browse/page.js
+++ app/browse/page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import BrowserCard from '../components/BrowserCard'; // Adjust the import path as necessary
+import './browse.css'
 
 // Images
 import friendsImage from '../../public/Images/Beige_Card.png'; 
@@ -28,7 +29,7 @@ const Browser = () => {
   return (
     <div className="container mx-auto px-4 py-8 ">
       <div className="text-center mb-12 ">
-        <h2 className="text-4xl font-bold mb-6 ">Current Decks</h2>
+        <h2 className="text-4xl font-bold mb-6 current-decks">Current Decks</h2>
         <p className="text-lg text-gray-600 ">Explore our collection of question decks to spark interesting conversations.</p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 ">
--- app/home/page.js
+++ app/home/page.js
@@ -5,7 +5,7 @@ import CardScroll from '../components/CardScroll';
 
 // Images
 import friendsImage from '../../public/Images/Beige_Card.png'; 
-import friendsImageBack from '../../public/Images/Beige_Card_Back.png'
+import friendsImageBack from '../../public/Images/Beige_Card_Back.png';
 import LoversImage from '../../public/Images/Lovers.png'; 
 import LoversImageBack from '../../public/Images/Lovers_Back.png'; 
 import MarriedImage from '../../public/Images/Married.png'; 
@@ -13,9 +13,6 @@ import GirlsImage from '../../public/Images/Girls.png';
 import BoysImage from '../../public/Images/Boys.png'; 
 import PurpleImage from '../../public/Images/Purple.png'; 
 import GreenImage from '../../public/Images/Light_green.png';
-
-
-import './HomePage.css';
 
 const cards = [
   { image: PurpleImage, alt: 'Purple' },
@@ -32,18 +29,20 @@ const cards = [
 
 const HomePage = () => {
   return (
-    <div className="homepage">
-      <div className="inline-container">
-        <div className="text-content">
-          <h2>Play for free</h2>
-          <p>Discover a variety of questions designed to spark interesting conversations and deepen your connections with friends and loved ones. Play for free or subscribe for unlimited access to more decks with deeper questions and different themes.</p>
+    <div className="container mx-auto p-4 pt-24">
+      <div className="flex flex-col lg:flex-row items-center justify-between mb-8">
+        <div className="lg:w-1/2 mb-4 lg:mb-0 text-center lg:text-left">
+          <h2 className="text-3xl font-bold mb-4">Play for free</h2>
+          <p className="text-lg">
+            Discover a variety of questions designed to spark interesting conversations and deepen your connections with friends and loved ones. Play for free or subscribe for unlimited access to more decks with deeper questions and different themes.
+          </p>
         </div>
-        <div className="card-links">
+        <div className="flex space-x-4">
           <CardLink to="/friends" frontImage={friendsImage} backImage={friendsImageBack} description="Questions for Friends" />
           <CardLink to="/lovers" frontImage={LoversImage} backImage={LoversImageBack} description="Questions for Lovers" />
         </div>
       </div>
-      <h2>Premium Decks</h2>
+      <h2 className="text-3xl font-bold mb-4 text-center">Premium Decks</h2>
       <CardScroll cards={cards} />
     </div>
   );
