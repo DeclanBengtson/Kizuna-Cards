c54ce2bca7d3a9e501400e6260bca47ba26cbeba
Declan Bengtson <n11079550@qut.edu.au>
2024-10-19 16:50:12 +1000

    Updated navbar dropdown and styling

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,4 +1,4 @@
-'use client'; // Add this directive at the top of the file
+'use client';
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
@@ -38,28 +38,30 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="navbar bg-base-100 shadow-lg">
-        <div className="navbar-start">
-          <Link href="/" className="btn btn-ghost normal-case text-xl">Couples Questions</Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li><Link href="/myo" className="btn btn-ghost">MYO</Link></li>
-            <li><Link href="/browse" className="btn btn-ghost">Browser</Link></li>
-            <li><Link href="/login" className="btn btn-ghost">Login</Link></li>
-            <li><button className="btn btn-ghost" onClick={openModal}>Premium</button></li>
-          </ul>
-        </div>
-        <div className="navbar-end lg:hidden">
-          <button className="btn btn-ghost btn-circle" onClick={toggleDropdown}>
-            <Image src={navigationIcon} alt="Menu" width={30} height={30} />
-          </button>
+      <nav className="navbar bg-base-100 shadow-lg fixed top-0 left-0 w-full z-50">
+        <div className="flex justify-between items-center w-full px-4">
+          <div className="flex-1">
+            <Link href="/" className="btn btn-ghost normal-case text-xl">Couples Questions</Link>
+          </div>
+          <div className="hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li><Link href="/myo" className="btn btn-ghost">MYO</Link></li>
+              <li><Link href="/browse" className="btn btn-ghost">Browse</Link></li>
+              <li><Link href="/login" className="btn btn-ghost">Login</Link></li>
+              <li><button className="btn btn-ghost" onClick={openModal}>Premium</button></li>
+            </ul>
+          </div>
+          <div className="lg:hidden">
+            <button className="btn btn-ghost btn-circle" onClick={toggleDropdown}>
+              <Image src={navigationIcon} alt="Menu" width={30} height={30} />
+            </button>
+          </div>
         </div>
         {isDropdownOpen && (
-          <div className="navbar-dropdown lg:hidden">
-            <ul className="menu menu-vertical bg-base-100 w-full p-2 shadow-lg">
+          <div className="absolute top-full left-0 w-full bg-base-100 shadow-lg lg:hidden">
+            <ul className="menu menu-vertical w-full p-2">
               <li><Link href="/myo" className="btn btn-ghost">MYO</Link></li>
-              <li><Link href="/browse" className="btn btn-ghost">Browser</Link></li>
+              <li><Link href="/browse" className="btn btn-ghost">Browse</Link></li>
               <li><Link href="/login" className="btn btn-ghost">Login</Link></li>
               <li><button className="btn btn-ghost" onClick={openModal}>Premium</button></li>
             </ul>
