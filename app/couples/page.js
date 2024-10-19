7501ac080b4fe2d7fb460c9c454769296f9923ab
Declan Bengtson <n11079550@qut.edu.au>
2024-10-19 19:14:48 +1000

    Added Background to couples page

--- app/couples/page.js
+++ app/couples/page.js
@@ -5,6 +5,7 @@ import FrontImage from '../../public/Images/Couples/Couples_Front.png';
 import BackImage from '../../public/Images/Couples/Couples_Back.png';
 
 import friendsQuestions from '../../questions/friendsQuestions.json';
+import Wallpaper from '../../public/Images/Backgrounds/Couples.png';
 
 const Lovers = () => {
   const initialCards = friendsQuestions.slice(0, 1).map((question) => ({
@@ -14,15 +15,13 @@ const Lovers = () => {
     zIndex: 1
   }));
 
-  const wallpaperUrl = '../../public/Images/Backgrounds/Couples.png'; // Update with your wallpaper path
 
   return (
     <div
       className="flex flex-col justify-center items-center h-screen w-full bg-cover bg-center"
-      style={{ backgroundImage: `url(${wallpaperUrl})` }}
+      style={{ backgroundImage: `url(${Wallpaper.src})`  }}
     >
       <Deck
-        title="Lovers Questions"
         initialCards={initialCards}
         questions={friendsQuestions}
         customStyles="lovers-card"
