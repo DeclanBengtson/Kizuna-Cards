04b73cab678e7c42e4bd4f18756a5eac1147b9c7
Declan Bengtson <n11079550@qut.edu.au>
2024-10-31 14:30:23 +1000

    Updated card content

--- app/browse/page.js
+++ app/browse/page.js
@@ -10,7 +10,7 @@ import WorkColleaguesImage from '../../public/Images/WorkColleagues/WorkColleagu
 import ChristmasImage from '../../public/Images/Christmas/Christmas_Front.png';
 
 const Decks = [
-  { to: '/lovers', image: LoversImage, description: 'Questions for Lovers' },
+  { to: '/couples', image: LoversImage, description: 'Questions for Couples' },
   { to: '/friends', image: friendsImage, description: 'Questions for Friends' },
   { to: '/family', image: familyImage, description: 'Questions for Family' },
   { to: '/newconnections', image: newConnectionsImage, description: 'Questions for New Connections' },
@@ -22,7 +22,7 @@ const Browser = () => {
   return (
     <div className="flex h-screen pt-24">
       {/* Static Content on the Left */}
-      <div className="w-1/3 bg-gray-100 p-8">
+      <div className="w-1/3 bg-gray-100 p-8 hidden lg:block">
         <h2 className="text-2xl font-bold mb-4">About Our Decks</h2>
         <p className="text-gray-700 mb-4">
           Our decks are designed to spark interesting conversations and deepen connections with friends, family, and colleagues.
@@ -33,7 +33,7 @@ const Browser = () => {
       </div>
 
       {/* Browser Cards on the Right */}
-      <div className="w-2/3 overflow-y-auto p-8">
+      <div className="w-full lg:w-2/3 overflow-y-auto p-8">
         <div className="text-center mb-8">
           <h2 className="text-4xl font-bold mb-4">Our Decks</h2>
           <p className="text-lg text-gray-600">Explore our collection of free question decks to spark interesting conversations.</p>
@@ -43,7 +43,6 @@ const Browser = () => {
             <BrowserCard key={index} to={deck.to} image={deck.image} description={deck.description} />
           ))}
         </div>
-
       </div>
     </div>
   );
