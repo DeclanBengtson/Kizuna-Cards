e8614e6011248a48f356a1c91735fd68cde06936
Declan Bengtson <n11079550@qut.edu.au>
2024-10-31 16:23:49 +1000

    typo for the footer import

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@
 import { SessionProvider } from 'next-auth/react';
 import './styles/globals.css';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/footer';
 import Head from 'next/head';
 
 export default function RootLayout({ children }) {
